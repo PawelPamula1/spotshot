4dea0d5d9fe30aa28714e8e080cf2536346191f9
Pawel Pamula <pawelpamula003@gmail.com>
2025-07-28 11:05:10 +0200

    increasing space on the bottom of the page

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -5,8 +5,10 @@ import { SpotGrid } from "@/components/ui/SpotGrid";
 import { getFavouriteSpots } from "@/lib/api/favourites";
 import { useAuth } from "@/provider/AuthProvider";
 import { Spot } from "@/types/spot"; // dostosuj ścieżkę
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Saved() {
+  const insets = useSafeAreaInsets();
   const { userId } = useAuth();
   const [savedSpots, setSavedSpots] = useState<Spot[]>([]);
   const [loading, setLoading] = useState(true);
@@ -34,14 +36,14 @@ export default function Saved() {
 
   if (!savedSpots.length) {
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, { paddingBottom: insets.bottom + 24 }]}>
         <Text style={styles.empty}>Nie masz jeszcze zapisanych miejsc.</Text>
       </View>
     );
   }
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { paddingBottom: insets.bottom + 24 }]}>
       <SpotGrid spots={savedSpots} />
     </View>
   );
